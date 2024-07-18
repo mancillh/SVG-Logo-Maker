82e675f762ca81ba2d368a16720e9c9d3ae3f18e
LeonathaTurtle <mancillh@gmail.com>
2024-07-18 16:33:58 -0500

    added logic for creating shapes, worked with coordinates in demo svg

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const content = `<svg version="1.1"
 
   <rect width="100%" height="100%" fill=${white} />
 
-  <${answers.shape} cx="150" cy="100" r="80" fill=${answers.bgColor} />
+  <${createShape()} fill=${answers.bgColor} />
 
   <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.textContent}</text>
 
